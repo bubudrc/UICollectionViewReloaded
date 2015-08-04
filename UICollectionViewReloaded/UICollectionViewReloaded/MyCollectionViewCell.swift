f454d060bd43e7ea1fd4876420271697bab452c6
Marcelo Perretta <mperretta@e-commla.com>
2015-08-04 20:17:20 -0300

    Added the basi model

---
+++ UICollectionViewReloaded/UICollectionViewReloaded/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  UICollectionViewReloaded
+//
+//  Created by marcelo.perretta@gmail.com on 8/4/15.
+//  Copyright (c) 2015 MAWAPE. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
--- UICollectionViewReloaded/UICollectionViewReloaded/ViewController.swift
+++ UICollectionViewReloaded/UICollectionViewReloaded/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let CellIdentifier = "Cell Identifier"
 
+class ViewController: UICollectionViewController {
+
+    var datesArray = [NSDate]()
+    let dateFormatter = NSDateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //1.
+        //instantiate our model
+        dateFormatter.dateFormat = "h:mm:ss";
+        dateFormatter.locale = NSLocale.currentLocale()
+        
+        //2.
+        //configure our collection view layout
+        let flowLayout = self.collectionView!.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.minimumInteritemSpacing = 40.0;
+        flowLayout.minimumLineSpacing = 40.0;
+        flowLayout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);
+        flowLayout.itemSize = CGSizeMake(200, 200);
+        
+        //3.
+        // Register cell classes
+        self.collectionView!.registerClass(MyCollectionViewCell.self, forCellWithReuseIdentifier: CellIdentifier)
+        self.collectionView!.indicatorStyle = .White
+        
+        //4.
+        //configure our navigation item
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "userTappedAddButton:")
+        self.navigationItem.rightBarButtonItem = addButton
+        self.navigationItem.title = "Our Time Machine"
+
     }
 
     override func didReceiveMemoryWarning() {
